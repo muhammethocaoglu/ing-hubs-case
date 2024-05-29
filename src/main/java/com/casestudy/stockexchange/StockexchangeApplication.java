202f4482a5a473498a0f33aef5a9b551734d4a84
muhammethocaoglu <muhammet@alumni.sabanciuniv.edu>
2024-05-29 20:23:09 +0300

    implements api key authentication

--- src/main/java/com/casestudy/stockexchange/StockexchangeApplication.java
+++ src/main/java/com/casestudy/stockexchange/StockexchangeApplication.java
@@ -2,8 +2,10 @@ package com.casestudy.stockexchange;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class})
 public class StockexchangeApplication {
 
     public static void main(String[] args) {
